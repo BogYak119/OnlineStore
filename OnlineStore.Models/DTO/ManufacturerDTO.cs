9622c217be4c39ccb3987fa7f562a9407871d30f
BogYak119 <bogdan.yakovenko119@gmail.com>
2022-11-18 17:09:11 +0200

    ManufacturerAPIController

---
+++ OnlineStore.Models/DTO/ManufacturerDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models.DTO
+{
+    public class ManufacturerDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string? StreetAddress { get; set; }
+        [MaxLength(50)]
+        public string? City { get; set; }
+        [MaxLength(50)]
+        public string? State { get; set; }
+        [MaxLength(10)]
+        public string? PostalCode { get; set; }
+        [MaxLength(20)]
+        public string? PhoneNumber { get; set; }
+        [MaxLength(50)]
+        public string? Email { get; set; }
+    }
+}
